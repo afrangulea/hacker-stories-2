6a5dbbec4f9670f010bb74728191781814752c60
Alexandru Frangulea <a.frangulea@gmail.com>
2023-10-20 22:28:04 +0300

    added multiple components, props and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import * as React from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+    {
+      title: "WordPress",
+      url: "https://redux.js.org/",
+      author: "Community",
+      num_comments: 12,
+      points: 7,
+      objectID: 2,
+    },
+  ];
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo2" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <Search />
+
+      <List list={stories} />
+    </div>
   );
-}
+};
+
+const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const handleChange = (event) => {
+    console.log(event);
+    setSearchTerm(event.target.value);
+  };
+  return (
+    <form>
+      <label htmlFor="input">Say hello: </label>
+      <input id="input" onChange={handleChange}></input>
+      <p>Your search term is {searchTerm}</p>
+    </form>
+  );
+};
+
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item item={item} key={item.objectID} />
+    ))}
+  </ul>
+);
+
+const Item = (props) => (
+  <li>
+    <span>Title: {props.item.title}</span>
+    <span>
+      <a href={props.item.url}>URL</a>
+    </span>
+    <span>Comments: {props.item.num_comments}</span>
+    <span>Points: {props.item.points}</span>
+    <br /> <br />
+  </li>
+);
 
 export default App;
